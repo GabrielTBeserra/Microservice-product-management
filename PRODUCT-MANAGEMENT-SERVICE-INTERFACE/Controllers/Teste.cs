96075960e0b0e5f78e8a2ae36fd0e304afe5b831
gabriel <gabrieltbeserra@gmail.com>
2022-02-23 15:19:49 -0300

    Alterado portas do mysql

---
+++ PRODUCT-MANAGEMENT-SERVICE-INTERFACE/Controllers/Teste.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PRODUCT_MANAGEMENT_SERVICE_INTERFACE.Controllers
+{
+
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    [Authorize]
+    public class Teste : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Asd() => Ok();
+
+    }
+}
